2cf2ddd209b953b0d14d8b518a16deed4c4b75b0
Yomna Hatem <yomnaahateem2001@gmail.com>
2025-06-09 18:30:58 +0300

    resolving

--- OstaFandy.PL/Mapper/MappingProfile.cs
+++ OstaFandy.PL/Mapper/MappingProfile.cs
@@ -39,33 +39,25 @@ namespace OstaFandy.PL.Mapper
                 CreateMap<Address, AddressDTO>().ReverseMap();
                 CreateMap<JobAssignment, JobAssignmentDTO>().ReverseMap();
 
-
+                #region BlockDate
                 CreateMap<BlockDate, AdminBlockDateDTO>()
                     .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
                     .ReverseMap();
-            
-            #region admin handyman 
+                #endregion
 
-            #region BlockDate
-            CreateMap<BlockDate, AdminBlockDateDTO>()
-                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
-                .ReverseMap();
-            #endregion
-
-
-            #region Handyman
-            CreateMap<Handyman, AdminHandyManDTO>()
-                .AfterMap((src, dest) =>
-                {
-                    if (src.User != null)
+                #region Handyman
+                CreateMap<Handyman, AdminHandyManDTO>()
+                    .AfterMap((src, dest) =>
                     {
-                        dest.FirstName = src.User.FirstName;
-                        dest.LastName = src.User.LastName;
-                        dest.Email = src.User.Email;
-                        dest.Phone = src.User.Phone;
-                        dest.IsActive = src.User.IsActive;
-                        dest.CreatedAt = src.User.CreatedAt;
-                    }
+                        if (src.User != null)
+                        {
+                            dest.FirstName = src.User.FirstName;
+                            dest.LastName = src.User.LastName;
+                            dest.Email = src.User.Email;
+                            dest.Phone = src.User.Phone;
+                            dest.IsActive = src.User.IsActive;
+                            dest.CreatedAt = src.User.CreatedAt;
+                        }
 
                         dest.UpdatedAt = src.UpdatedAt;
                         dest.Latitude = src.Latitude;
@@ -91,61 +83,29 @@ namespace OstaFandy.PL.Mapper
                         }).ToList();
                     });
 
-
-                        dest.AdminBlockDateDTO = src.BlockDates?.Select(bd => new AdminBlockDateDTO
+                CreateMap<EditHandymanDTO, Handyman>()
+                    .AfterMap((src, dest) =>
+                    {
+                        if (dest.User != null)
                         {
-                            Id = bd.Id,
-                            UserId = bd.UserId,
-                            Reason = bd.Reason,
-                            StartDate = bd.StartDate,
-                            EndDate = bd.EndDate,
-                            IsActive = bd.IsActive,
-                            CreatedAt = bd.CreatedAt,
-                            UpdatedAt = bd.UpdatedAt
-                        }).ToList();
-                    });
-            }
-            CreateMap<EditHandymanDTO, Handyman>()
-               .AfterMap((src, dest) =>
-               {
-                   if (dest.User != null)
-                   {
-                       dest.User.FirstName = src.FirstName;
-                       dest.User.LastName = src.LastName;
-                       dest.User.Email = src.Email;
-                       dest.User.Phone = src.Phone;
-                       dest.User.UpdatedAt = DateTime.UtcNow;
-                   }
-               })
-               .ReverseMap();
-            #endregion
+                            dest.User.FirstName = src.FirstName;
+                            dest.User.LastName = src.LastName;
+                            dest.User.Email = src.Email;
+                            dest.User.Phone = src.Phone;
+                            dest.User.UpdatedAt = DateTime.UtcNow;
+                        }
+                    })
+                    .ReverseMap();
+                #endregion
 
-            #region admin client
-
-            CreateMap<EditHandymanDTO, Handyman>()
-                .AfterMap((src, dest) =>
-                {
-                    if (dest.User != null)
+                #region Client
+                CreateMap<User, AdminDisplayClientDTO>()
+                    .AfterMap((src, dest) =>
                     {
-                        dest.User.FirstName = src.FirstName;
-                        dest.User.LastName = src.LastName;
-                        dest.User.Email = src.Email;
-                        dest.User.Phone = src.Phone;
-                        dest.User.UpdatedAt = DateTime.UtcNow;
-                    }
-                })
-                .ReverseMap();
-            #endregion
-
-            #region Client
-
-            CreateMap<User, AdminDisplayClientDTO>()
-                .AfterMap((src, dest) =>
-                {
-                    if (src.Client != null)
-                    {
-                        dest.ClientUserId = src.Client.UserId;
-                        dest.DefaultAddressId = src.Client.DefaultAddressId;
+                        if (src.Client != null)
+                        {
+                            dest.ClientUserId = src.Client.UserId;
+                            dest.DefaultAddressId = src.Client.DefaultAddressId;
 
                             if (src.Client.DefaultAddress != null)
                             {
@@ -188,44 +148,23 @@ namespace OstaFandy.PL.Mapper
                         }
                     });
 
-            CreateMap<AdminEditClientDTO, User>()
-                .AfterMap((src, dest) =>
-                {
-                    if (dest.Client != null)
+                CreateMap<AdminEditClientDTO, User>()
+                    .AfterMap((src, dest) =>
                     {
-                        dest.Client.DefaultAddressId = src.DefaultAddressId;
-                        dest.UpdatedAt = DateTime.UtcNow;
-                    }
-                })
-                .ReverseMap();
+                        if (dest.Client != null)
+                        {
+                            dest.Client.DefaultAddressId = src.DefaultAddressId;
+                            dest.UpdatedAt = DateTime.UtcNow;
+                        }
+                    })
+                    .ReverseMap();
+                #endregion
 
-
-            #endregion
-
-            #region feedback admin page
-            CreateMap<Review, OrderFeedbackDto>()
-                .AfterMap((src, dest) =>
-                {
-                    dest.BookingId = src.BookingId;
-                    dest.HandymanName = $"{src.Booking.JobAssignment.Handyman.User.FirstName}  {src.Booking.JobAssignment.Handyman.User.LastName}";
-                    dest.HandymanSpecialty = src.Booking.JobAssignment.Handyman.Specialization?.Name;
-                    dest.ClientName = $"{src.Booking.Client.User.FirstName} {src.Booking.Client.User.LastName}";
-                    dest.ServiceName = src.Booking.JobAssignment.Handyman.Specialization.Name;
-                    dest.Rating = src.Rating;
-                    dest.Comment = src.Comment;
-                    dest.CompletedAt = src.Booking.JobAssignment.CompletedAt;
-                    dest.ReviewCreatedAt = src.CreatedAt;
-
-                })
-                .ReverseMap();
-
-            #endregion
-
-            #region Payment
-            CreateMap<Payment, PaymentDto>()
-                .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src =>
-                    $"{src.Booking.Client.User.FirstName} {src.Booking.Client.User.LastName}"))
-                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.CreatedAt));
+                #region Payment
+                CreateMap<Payment, PaymentDto>()
+                    .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src =>
+                        $"{src.Booking.Client.User.FirstName} {src.Booking.Client.User.LastName}"))
+                    .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.CreatedAt));
 
                 CreateMap<Payment, PaymentDetailsDto>()
                     .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src =>
@@ -234,18 +173,19 @@ namespace OstaFandy.PL.Mapper
                     .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.CreatedAt));
                 #endregion
 
-            #region Booking
-            CreateMap<Booking, BookingViewDto>()
-           .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src => $"{src.Client.User.FirstName} {src.Client.User.LastName}"))
-           .ForMember(dest => dest.HandymanName, opt => opt.MapFrom(src =>
-               src.JobAssignment != null && src.JobAssignment.Handyman != null
-                   ? $"{src.JobAssignment.Handyman.User.FirstName} {src.JobAssignment.Handyman.User.LastName}"
-                   : string.Empty))
-           .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src =>
-               src.BookingServices.FirstOrDefault().Service.Category.Name))
-           .ForMember(dest => dest.ServiceNames, opt => opt.MapFrom(src =>
-               src.BookingServices.Select(bs => bs.Service.Name).ToList()));
-            #endregion
+                #region Booking
+                CreateMap<Booking, BookingViewDto>()
+               .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src => $"{src.Client.User.FirstName} {src.Client.User.LastName}"))
+               .ForMember(dest => dest.HandymanName, opt => opt.MapFrom(src =>
+                   src.JobAssignment != null && src.JobAssignment.Handyman != null
+                       ? $"{src.JobAssignment.Handyman.User.FirstName} {src.JobAssignment.Handyman.User.LastName}"
+                       : string.Empty))
+               .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src =>
+                   src.BookingServices.FirstOrDefault().Service.Category.Name))
+               .ForMember(dest => dest.ServiceNames, opt => opt.MapFrom(src =>
+                   src.BookingServices.Select(bs => bs.Service.Name).ToList()));
+                #endregion
+            }
         }
     }
 }
