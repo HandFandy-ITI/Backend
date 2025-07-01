a0a69b9836c33c9f613f9a62f5529741f438941b
Fatma <fatmaelsheikh958@gmail.com>
2025-07-01 18:43:27 +0300

    update client profile DTOs

--- OstaFandy.PL/DTOs/ClientReviewDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OstaFandy.PL.DTOs
-{
-    public class ClientReviewDTO
-    {
-        public byte Rating { get; set; }
-        public string? Comment { get; set; }
-        public DateTime ReviewDate { get; set; }
-    }
-}
